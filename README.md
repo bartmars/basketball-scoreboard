2a24252280564562824e3149c82912468ca208de
bartmars <bart.mars@outlook.com>
2023-01-09 13:56:28 +0100

    update project

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ Bart Mars
 ## Deployment
 This repository is being auto-deployed via a [Netlify app](https://).
 
-TODO: Netlify status badge
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3339e188-2bad-4e03-90fe-e26fe3ef25c4/deploy-status)](https://app.netlify.com/sites/inquisitive-bonbon-46740a/deploys)
 
 ## Requirements 
 * Build it from "scratch"
 * Needs to use:
-    * [ ] Follow the [Figma design](https://www.figma.com/file/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0%3A1&t=gz7FjqNfCTW91FnR-0)
-    * [ ] Make all six buttons (+1, +2, +3) work:
-        * [ ] Home team
-        * [ ] Guest team
+    * [x] Follow the [Figma design](https://www.figma.com/file/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0%3A1&t=gz7FjqNfCTW91FnR-0)
+    * [x] Make all six buttons (+1, +2, +3) work:
+        * [x] Home team
+        * [x] Guest team
 * Stretch goals:
-    * Add a "New Game" button
-    * Highlight the leading team
-    * Add a few more counters (e.g. period, fouls, timer)
+    * [x] Add a "New Game" button
+    * [x] Highlight the leading team
+    * [ ] Add a few more counters (e.g. period, fouls, timer)
--- index.js
+++ index.js
@@ -1,23 +1,101 @@
-let homeTeamScore = 0;
-let guestTeamScore = 0;
+let homeScore = 0;
+let homeFouls = 0;
+let homeScoreEl = document.getElementById("home-score-el");
+let homeFoulEl = document.getElementById("home-foul-el");
 
-let homeTeamEl = document.getElementById("hometeam-score-el");
-let guestTeamEl = document.getElementById("guestteam-score-el");
+let visitorScore = 0;
+let visitorFouls = 0;
+let visitorScoreEl = document.getElementById("visitor-score-el");
+let visitorFoulEl = document.getElementById("visitor-foul-el");
 
-function add1Point() {
-    console.log("Button clicked, score updated by 1.");
-    homeTeamScore += 1;
-    homeTeamEl.textContent = homeTeamScore;
+let period = 1;
+let periodTimer = 0;
+let periodEl = document.getElementById("period-el");
+let periodTimerEl = document.getElementById("period-timer-el");
+
+function addHome1Point() {
+    homeScore += 1;
+    homeScoreEl.textContent = homeScore;
+    highlightWinningTeam();
 }
 
-function add2Points() {
-    console.log("Button clicked, score updated by 2.");
-    homeTeamScore += 2;
-    homeTeamEl.textContent = homeTeamScore;
+function addHome2Points() {
+    homeScore += 2;
+    homeScoreEl.textContent = homeScore;
+    highlightWinningTeam();
 }
 
-function add3Points() {
-    console.log("Button clicked, score updated by 2.");
-    homeTeamScore += 3;
-    homeTeamEl.textContent = homeTeamScore;
+function addHome3Points() {
+    homeScore += 3;
+    homeScoreEl.textContent = homeScore;
+    highlightWinningTeam();
 }
+
+function addVisitor1Point() {
+    visitorScore += 1;
+    visitorScoreEl.textContent = visitorScore;
+    highlightWinningTeam();   
+}
+
+function addVisitor2Points() {
+    visitorScore += 2;
+    visitorScoreEl.textContent = visitorScore;
+    highlightWinningTeam();
+}
+
+function addVisitor3Points() {
+    visitorScore += 3;
+    visitorScoreEl.textContent = visitorScore;
+    highlightWinningTeam();
+}
+
+function addHomeFoul() {
+    homeFouls += 1;
+    homeFoulEl.textContent = homeFouls;
+}
+
+function addVisitorFoul() {
+    visitorFouls += 1;
+    visitorFoulEl.textContent = visitorFouls;
+}
+
+function addPeriod() {
+    period += 1;
+    periodEl.textContent = period;
+    periodTimer = "10:00";
+    periodTimerEl.textContent = periodTimer;
+}
+
+function resetGame() {
+    homeScore = 0;
+    homeFouls = 0;
+    homeScoreEl.textContent = homeScore;
+    homeFoulEl.textContent = homeFouls;
+    homeScoreEl.style.color = "#9f1239";
+    visitorScore = 0;
+    visitorFouls = 0;
+    visitorScoreEl.textContent = visitorScore;
+    visitorFoulEl.textContent = visitorFouls;
+    visitorScoreEl.style.color = "#9f1239";
+    period = 1;
+    periodEl.textContent = period;
+    periodTimer = "10:00";
+    periodTimerEl = periodTimer;
+}
+
+function highlightWinningTeam() {
+    if (homeScore > visitorScore) {
+        homeScoreEl.style.color = "yellow";
+        visitorScoreEl.style.color = "#F94F6D";
+    } else if (homeScore == visitorScore) {
+        homeScoreEl.style.color = "#F94F6D";
+        visitorScoreEl.style.color = "#F94F6D";
+    } else {
+        visitorScoreEl.style.color = "yellow";
+        homeScoreEl.style.color = "#F94F6D";
+    }
+}
+
+// function periodTimer() {
+
+// }
